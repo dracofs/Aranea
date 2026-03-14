93df7a3ca4d95f23c26bb9ca6410c85af07cd49b
lferre03 <lferre03@student.ubc.ca>
2026-03-14 00:58:48 -0700

    added crawler main loop

--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -2,7 +2,11 @@ package crawler
 
 import (
 	"ARANEA/internal/dedupe"
+	"ARANEA/internal/fetcher"
 	"ARANEA/internal/frontier"
+	"ARANEA/internal/parser"
+	"ARANEA/internal/utils"
+	"fmt"
 )
 
 type Crawler struct {
@@ -17,4 +21,50 @@ func newCrawler(seed string) *Crawler {
 	q.Push(seed)
 
 	return &Crawler{queue: q, visited: v}
+}
+
+func (c *Crawler) Crawl() {
+	// main crawl loop, structure follows the outline below:
+	/*
+	 - get url from queue, mark as visited
+	 - fetch page
+	 - parse page for links, add to queue if not visited
+	 - stop after a certain depth is reached or queue is empty
+	*/
+
+	for {
+		curr, flag := c.queue.Pop()
+
+		if !flag {
+			break
+		}
+
+		c.visited.Insert(curr)
+		fmt.Println("Crawling:", curr)
+
+		content, err := fetcher.Fetch(curr)
+
+		if err != nil {
+			continue
+		}
+
+		links, err := parser.GetLinks(content)
+
+		if err != nil {
+			continue
+		}
+
+		for _, link := range links {
+			normalized, err := utils.Normalize(curr, link)
+
+			if err != nil {
+				continue
+			}
+
+			if !c.visited.Seen(normalized) {
+				c.queue.Push(normalized)
+			}
+		}
+
+	}
 }
