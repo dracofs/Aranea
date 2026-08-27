198237a83f440286f5363819193762e3c0dfa2b5
dracofs <dracofs.777@gmail.com>
2026-08-26 23:38:38 -0700

    modified entry point to allow users to enter their own starting seeds + number of workers

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"ARANEA/internal/crawler"
+	"ARANEA/internal/utils"
+	"flag"
+	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
-	seed := "https://redis.io/"
-	workers := 5
+	seed := flag.String("seed", "", "starter URL to crawl (http or https)")
+	workers := flag.Int("workers", 5, "number of crawl workers")
+	flag.Parse()
 
-	c, err := crawler.NewCrawler(seed)
+	seedURL := *seed
+	if seedURL == "" {
+		fmt.Fprintf(os.Stderr, "usage: %s -seed <url>\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	normalized, err := utils.Normalize(seedURL, seedURL)
+	if err != nil {
+		log.Fatalf("seed must be an absolute http(s) URL, got %q: %v", seedURL, err)
+	}
+
+	c, err := crawler.NewCrawler(normalized)
 	if err != nil {
 		log.Fatalf("crawler init: %v", err)
 	}
-	c.Start(workers)
+	c.Start(*workers)
 }
